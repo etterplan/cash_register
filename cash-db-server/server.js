8583fe06b587ff5108d782419b730aba85c7085f
Thomas Lawenius <Thomas.Lawenius@etteplan.com>
2024-05-28 14:56:21 +0200

    Add table BarAccount and save routine in server.js

--- cash-db-server/server.js
+++ cash-db-server/server.js
@@ -10,6 +10,9 @@ const prisma = new PrismaClient();
 app.use(cors());
 app.use(express.json());
 
+prisma.$on('query', e => {
+  console.log(e);
+});
 
 app.get('/guests', async (req, res) => {
   try {
@@ -28,6 +31,23 @@ app.get('/articles', async (req, res) => {
   } catch (error) {
     console.error('Articles: Error retrieving data: ', error);
     res.status(500).send('Articles: Internal Server Error');
+  }
+});
+
+app.post('/baraccount', async (req, res) => {
+  const { guest_id, article } = req.body;
+  try {
+    const invoice = await prisma.barAccount.create({
+      data: {
+        guest_id,
+        article
+      }
+    });
+    
+    res.json(invoice);
+  } catch (error) {
+    console.error('BarAccount: Error creating data: ', error);
+    res.status(500).send('BarAccount: Internal Server Error - ' + error.message);
   }
 });
 
